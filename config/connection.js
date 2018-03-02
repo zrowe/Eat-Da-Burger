95c1751ea7e8ee2e3a853084b7ea1888a6bfa5e6
zrowe <paul.louis.rowe@gmailcom>
2018-03-01 23:24:18 -0800

    adding connection and orm driver

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// require("dotenv").config(); // get mySQL credentials
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: "cat_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,4 @@
+require("dotenv").config(); // get mySQL credentials
+var express = require("express");
+var bodyParser = require("body-parser");
+
